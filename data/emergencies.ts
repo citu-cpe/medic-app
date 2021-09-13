e49a986381ecf533480e96b8658232ff66b1f65b
Princh Canal <princhcanal@gmail.com>
2021-09-13 17:46:48 +0800

    feat(first aid): add self defense

--- data/emergencies.ts
+++ data/emergencies.ts
@@ -14,6 +14,43 @@ export const emergenciesList: EmergencyListItem[] = [
       },
     ],
   },
-  { title: 'Open wound', icon: 'opacity', steps: [] },
-  { title: 'Nauseous', icon: 'sick', steps: [] },
+  {
+    title: 'Open wound',
+    icon: 'opacity',
+    steps: [
+      {
+        stepName: 'Example Step',
+        stepDescription:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        stepImageUrl:
+          'https://previews.123rf.com/images/fordzolo/fordzolo1506/fordzolo150600296/41026708-example-white-stamp-text-on-red-backgroud.jpg',
+      },
+    ],
+  },
+  {
+    title: 'Nauseous',
+    icon: 'sick',
+    steps: [
+      {
+        stepName: 'Example Step',
+        stepDescription:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        stepImageUrl:
+          'https://previews.123rf.com/images/fordzolo/fordzolo1506/fordzolo150600296/41026708-example-white-stamp-text-on-red-backgroud.jpg',
+      },
+    ],
+  },
+  {
+    title: 'Self Defense',
+    icon: 'sports-mma',
+    steps: [
+      {
+        stepName: 'Example Step',
+        stepDescription:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        stepImageUrl:
+          'https://previews.123rf.com/images/fordzolo/fordzolo1506/fordzolo150600296/41026708-example-white-stamp-text-on-red-backgroud.jpg',
+      },
+    ],
+  },
 ];
