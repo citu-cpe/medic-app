1470e2fd27ac314b5a0912d39a695c183f2fb4c8
Princh Canal <princhcanal@gmail.com>
2021-10-08 19:59:41 +0800

    test change

--- data/emergencies.ts
+++ data/emergencies.ts
@@ -53,4 +53,17 @@ export const emergenciesList: EmergencyListItem[] = [
       },
     ],
   },
+  {
+    title: 'New ',
+    icon: 'sports-mma',
+    steps: [
+      {
+        stepName: 'Example Step',
+        stepDescription:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        stepImageUrl:
+          'https://previews.123rf.com/images/fordzolo/fordzolo1506/fordzolo150600296/41026708-example-white-stamp-text-on-red-backgroud.jpg',
+      },
+    ],
+  },
 ];
