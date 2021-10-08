da62df5e8119347099a48021cdef5e50640dc259
Dina <dinagenedelatorre@gmail.com>
2021-10-08 20:25:09 +0800

    test

--- data/emergencies.ts
+++ data/emergencies.ts
@@ -15,26 +15,50 @@ export const emergenciesList: EmergencyListItem[] = [
     ],
   },
   {
-    title: 'Open wound',
+    title: 'Bleeding',
     icon: 'opacity',
     steps: [
       {
         stepName: 'Example Step',
         stepDescription:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          `First Aid for Bleeding
+          If you're faced with bleeding:
+          
+          1.Rinse the wound with water if possible.
+          2.Cover the wound with a gauze or cloth (towel, blanket, clothing, whatever's available).
+          3.Apply direct pressure to stop the blood flow and encourage clotting.
+          4.If possible, elevate the bleeding body part above the heart.
+          5.Don't remove the cloth if it becomes soaked through, but add more layers if needed. Removing the first layer will interfere with the clotting process and result in more blood loss.
+          6.Once bleeding is stopped, apply a clean bandage.`,
         stepImageUrl:
           'https://previews.123rf.com/images/fordzolo/fordzolo1506/fordzolo150600296/41026708-example-white-stamp-text-on-red-backgroud.jpg',
       },
     ],
   },
   {
-    title: 'Nauseous',
+    title: 'Choking',
     icon: 'sick',
     steps: [
       {
         stepName: 'Example Step',
         stepDescription:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          `First Aid for Choking
+          To perform the Heimlich maneuver:
+          
+          1.Stand behind the person and lean them slightly forward
+          2.Put your arms around their waist
+          3.Clench a fist and place it between their navel and rib cage
+          4.Grab your fist with your other hand
+          5.Pull the clenched fist sharply backward and upward under their rib cage in 5 quick thrusts. Repeat until the object is coughed up.
+          
+          For someone who's obese or pregnant, perform thrusts around the chest instead of the abdomen.
+          
+          If someone is unconscious:
+          
+          1.Place them on their back and kneel over them
+          2.Place the heel of your hand slightly above the navel
+          3.Place your other hand on top of it
+          4.Give quick upward thrusts to dislodge the obstruction`,
         stepImageUrl:
           'https://previews.123rf.com/images/fordzolo/fordzolo1506/fordzolo150600296/41026708-example-white-stamp-text-on-red-backgroud.jpg',
       },
@@ -47,7 +71,182 @@ export const emergenciesList: EmergencyListItem[] = [
       {
         stepName: 'Example Step',
         stepDescription:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+        stepImageUrl:
+          'https://previews.123rf.com/images/fordzolo/fordzolo1506/fordzolo150600296/41026708-example-white-stamp-text-on-red-backgroud.jpg',
+      },
+    ],
+  },
+  {
+    title: 'Cardiac Arrest',
+    icon: '',
+    steps: [
+      {
+        stepName: 'Example Step',
+        stepDescription:
+          `First Aid for Suspected Cardiac Arrest
+          When you suspect someone is in cardiac arrest:
+          
+          1.Get someone to call 911 or the medical alert system for your location.
+          2.Start chest compressions immediately. Push down hard and fast in the center of the chest, allowing the chest to come back up naturally between compressions. Continue until someone with more training has arrived.
+          3.If you're trained in CPR, use chest compressions and rescue breathing.
+          4.Use an AED if one is available. It's important to not delay chest compressions, though, so have someone else find one while you do chest compressions.`,
+      }
+    ]
+  },
+  {
+    title: 'Blisters',
+    icon: 'sports-mma',
+    steps: [
+      {
+        stepName: 'Example Step',
+        stepDescription:
+          `First Aid for Blisters
+          If the blister is large or painful—especially if the activity that caused it isn’t finished (such as if you are in the middle of a hike)—follow these steps to drain and dress a blister
+          
+          1.Wash your hands and sterilize a needle with alcohol.
+          2.Make small punctures at the edge of the blister.
+          3.Gently push the fluid out.
+          4.Apply antibiotic ointment.
+          5.Bandage it.
+          6.If possible, take steps to protect the area from further rubbing or pressure.
+          
+          If the blister broke open on its own:
+          
+          1.Gently wash with clean water only.
+          2.Smooth the flap of broken skin over the newly exposed skin, unless it's dirty, torn, or pus has gathered under it.
+          3.Apply petroleum jelly.
+          4.Bandage it.
+
+          Change the bandage any time it gets wet. Take it off when you go to bed so the area can dry. `,
+        stepImageUrl:
+          'https://previews.123rf.com/images/fordzolo/fordzolo1506/fordzolo150600296/41026708-example-white-stamp-text-on-red-backgroud.jpg',
+      },
+    ],
+  },
+  {
+    title: 'Burns',
+    icon: 'sports-mma',
+    steps: [
+      {
+        stepName: 'Example Step',
+        stepDescription:
+          `First Aid for Burns
+          Take these first aid steps:
+
+          1.Flush the burned area with cool running water for several minutes. Do not use ice.
+          2.Apply a light gauze bandage.
+          3.Take ibuprofen or acetaminophen for pain relief if necessary.
+          4.Do not break any blisters that may have formed.
+          
+          Do not apply ointments, butter, or oily remedies to a burn.Do not apply ointments, butter, or oily remedies to a burn.`,
+        stepImageUrl:
+          'https://previews.123rf.com/images/fordzolo/fordzolo1506/fordzolo150600296/41026708-example-white-stamp-text-on-red-backgroud.jpg',
+      },
+    ],
+  },
+  {
+    title: 'Broken Bone or Fracture',
+    icon: 'sports-mma',
+    steps: [
+      {
+        stepName: 'Example Step',
+        stepDescription:
+          `First Aid for Suspected Fracture
+          Take these steps for a suspected fracture:
+          
+          1.Don't try to straighten it.
+          2.For a limb, use a splint and padding to immobilize it.
+          3.Put a cold pack on the injury, with a barrier between it and the skin to prevent tissue damage. If ice is all that's available, put it in a plastic bag and wrap it in a shirt or towel.
+          4.If it's an extremity, elevate it.
+          5.Give anti-inflammatory drugs like Advil (ibuprofen) or Aleve (naproxen) for pain. `,
+        stepImageUrl:
+          'https://previews.123rf.com/images/fordzolo/fordzolo1506/fordzolo150600296/41026708-example-white-stamp-text-on-red-backgroud.jpg',
+      },
+    ],
+  },
+  {
+    title: '',
+    icon: 'Sprain',
+    steps: [
+      {
+        stepName: 'Example Step',
+        stepDescription:
+          `First Aid for Sprains
+          For a suspected sprain:
+          
+          1.Immobilize the limb
+          2.Apply a cold pack
+          3.Elevate the injured part if you can do so safely
+          4.Use NSAIDs for pain
+          
+          See your healthcare provider soon for further diagnosis and treatment. `,
+        stepImageUrl:
+          'https://previews.123rf.com/images/fordzolo/fordzolo1506/fordzolo150600296/41026708-example-white-stamp-text-on-red-backgroud.jpg',
+      },
+    ],
+  },
+  {
+    title: 'Nosebleeds',
+    icon: 'sports-mma',
+    steps: [
+      {
+        stepName: 'Example Step',
+        stepDescription:
+          `First Aid for Nosebleed
+          The first aid for nosebleed includes:
+          
+          1.Lean slightly forward, not back.
+          2.Pinch the nose just below the bridge, high enough that the nostrils aren't pinched closed.
+          3.Check after five minutes to see if bleeding has stopped. If not, continue pinching and check after another 10 minutes.
+          4.You can also apply a cold pack to the bridge of the nose while pinching. `,
+        stepImageUrl:
+          'https://previews.123rf.com/images/fordzolo/fordzolo1506/fordzolo150600296/41026708-example-white-stamp-text-on-red-backgroud.jpg',
+      },
+    ],
+  },
+  {
+    title: 'Frostbite',
+    icon: 'sports-mma',
+    steps: [
+      {
+        stepName: 'Example Step',
+        stepDescription:
+          `First Aid for Frostbite
+          Treating frostbite is a delicate procedure of gradual warming. If at all possible, this should be done by professionals at a medical facility. If that's not possible, or while awaiting an ambulance, you can begin first aid:
+          
+          1.Get out of the cold.
+          2.Immerse the affected area in warm water (98 to 105 F) for 20 to 30 minutes.
+          3.Do not rub the affected area.
+          4.Do not use sources of dry heat (like a heating pad or fire air).
+          5.For fingers and toes: After they're warm, put clean cotton balls between them.
+          6.Loosely wrap the area with bandages.
+          7.Use Tylenol or Advil for pain.
+          8.Get medical attention as soon as possible.
+          
+          For small areas of minor frostbite, you can also warm the area with skin-to-skin contact.
+          
+          Get emergency treatment if the skin is hard and begins turning white. `,
+        stepImageUrl:
+          'https://previews.123rf.com/images/fordzolo/fordzolo1506/fordzolo150600296/41026708-example-white-stamp-text-on-red-backgroud.jpg',
+      },
+    ],
+  },
+  {
+    title: 'Bee Stings',
+    icon: 'sports-mma',
+    steps: [
+      {
+        stepName: 'Example Step',
+        stepDescription:
+          `First Aid for Bee Stings
+          Follow these steps for bee sting first aid:
+          
+          1.Get the stinger out any way you can to keep it from injecting more venom. The method doesn't matter—it's most important to do it quickly.
+          2.Wash the area with soap and water.
+          3.Use a cold pack to reduce swelling at the site, but don't apply ice directly to the skin. 
+          4.Use an antihistamine like Benadryl (diphenhydramine) to reduce swelling and itching. 
+          5.Use Advil or Tylenol for pain. `,
         stepImageUrl:
           'https://previews.123rf.com/images/fordzolo/fordzolo1506/fordzolo150600296/41026708-example-white-stamp-text-on-red-backgroud.jpg',
       },
