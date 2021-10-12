728b8c9dba5fb91cd2e2cfbb6d7d2a12aa2dbb2b
Dina <dinagenedelatorre@gmail.com>
2021-10-12 15:57:41 +0800

    adding self defense in first aid

--- data/emergencies.ts
+++ data/emergencies.ts
@@ -7,11 +7,57 @@ export const emergenciesList: EmergencyListItem[] = [
     icon: 'coronavirus',
     steps: [
       {
-        stepName: 'Example Step',
-        stepDescription:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        stepName: 'What to do for suspected COVID-19 virus',
+        stepDescription: dedent`COVID 19
+          What to do if you think you have Coronavirus
+          https://firstaidforlife.org.uk/coronavirus-recovery/
+
+          How to care for someone who might have Coronavirus
+          https://onlinefirstaid.com/how-to-care-for-someone-who-might-have-coronavirus-covid-19/
+          
+          
+          Advice for first aiders
+          As a first aider, in addition to high quality clinical skills, to be effective you need to:
+          1. Be aware of the risks to yourself and others
+          2. Keep yourself safe
+          3. Give early treatment
+          4. Keep yourself informed and updated
+          5. Remember your own needs
+          
+
+          In the current climate with the prevalence of the COVID-19 pandemic, these skills become even more important
+          and here is how you can apply these skills when managing a first aid incident.
+          Our guidance
+          
+          
+          1. Be aware of the risks to yourself and others
+          When approaching a casualty there is always a risk of cross contamination – especially when you may have to get close to the casualty to assess what is wrong or to check their breathing. It is always important to be aware of the risks of how this cross contamination has occurred. According to NHS 111 we do not know exactly how coronavirus spreads from person to person but similar viruses are spread in cough droplets.
+          
+          
+          2. Keep yourself safe
+          In line with government advice, make sure you wash your hands or use an alcohol gel, before and after treating a casualty also ensure that you don’t cough or sneeze over a casualty when you are treating them.
+          
+          The Resuscitation Council (UK) provides some useful advice of how to keep yourself safe when providing CPR. You can read their full advice on their website here.
+          
+          Don’t lose sight of other cross contamination that could occur that isn’t related to COVID-19.
+            o Wear gloves or cover hands when dealing with open wounds
+            o Cover cuts and grazes on your hands with waterproof dressing
+            o Dispose of all waste safely
+            o Do not touch a wound with your bare hand
+            o Do not touch any part of a dressing that will come in contact with a wound.
+          
+          
+          3. Give early treatment
+          The vast majority of incidents do not involve you getting close to a casualty where you would come into contact with cough droplets. Sensible precautions will ensure you are able to treat a casualty effectively.
+          
+          
+          4. Keep yourself informed and updated
+          As this is a new disease this is an ever changing situation and the government and DOH are continually updating their advice.
+          
+          5. Remember your own needs
+          These are challenging and uncertain times for all. The COVID-19 outbreak has meant a lot of upheaval and worry for people. In order to help others you will also need to look after your own needs. Make sure you take time to talk about your fears and concerns with someone you trust and to take out time to look after yourself.`,
         stepImageUrl:
-          'https://previews.123rf.com/images/fordzolo/fordzolo1506/fordzolo150600296/41026708-example-white-stamp-text-on-red-backgroud.jpg',
+          'https://www.fda.gov/files/Coronavirus_3D_illustration_by_CDC_1600x900.png',
       },
     ],
   },
@@ -20,7 +66,7 @@ export const emergenciesList: EmergencyListItem[] = [
     icon: 'opacity',
     steps: [
       {
-        stepName: 'Example Step',
+        stepName: 'First Aid for Bleeding',
         stepDescription: dedent`First Aid for Bleeding
           If you're faced with bleeding:
           
@@ -29,7 +75,11 @@ export const emergenciesList: EmergencyListItem[] = [
           3.Apply direct pressure to stop the blood flow and encourage clotting.
           4.If possible, elevate the bleeding body part above the heart.
           5.Don't remove the cloth if it becomes soaked through, but add more layers if needed. Removing the first layer will interfere with the clotting process and result in more blood loss.
-          6.Once bleeding is stopped, apply a clean bandage.`,
+          6.Once bleeding is stopped, apply a clean bandage.
+          
+          
+          Source: https://www.verywellhealth.com/basic-first-aid-procedures-1298578
+          `,
         stepImageUrl:
           'https://www.verywellhealth.com/thmb/_Qe5EmFvRXsD8ASz3ED9Rbic8PQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-525386797-5a6a1eccfa6bcc003690b97f.jpg',
       },
@@ -40,7 +90,7 @@ export const emergenciesList: EmergencyListItem[] = [
     icon: 'sick',
     steps: [
       {
-        stepName: 'Example Step',
+        stepName: 'First Aid for Choking',
         stepDescription: dedent`First Aid for Choking
           To perform the Heimlich maneuver:
           
@@ -57,21 +107,13 @@ export const emergenciesList: EmergencyListItem[] = [
           1.Place them on their back and kneel over them
           2.Place the heel of your hand slightly above the navel
           3.Place your other hand on top of it
-          4.Give quick upward thrusts to dislodge the obstruction`,
+          4.Give quick upward thrusts to dislodge the obstruction
+          
+          
+          Source: https://www.verywellhealth.com/basic-first-aid-procedures-1298578
+          `,
         stepImageUrl:
           'https://www.verywellhealth.com/thmb/eez_1vIVOEa9Qr3UxA4xJdHH7F4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-136811278-664fedac86fc431d84ab156fdfeda4e4.jpg',
-      },
-    ],
-  },
-  {
-    title: 'Self Defense',
-    icon: 'sports-mma',
-    steps: [
-      {
-        stepName: 'Example Step',
-        stepDescription: dedent`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-        stepImageUrl:
-          'https://previews.123rf.com/images/fordzolo/fordzolo1506/fordzolo150600296/41026708-example-white-stamp-text-on-red-backgroud.jpg',
       },
     ],
   },
@@ -87,7 +129,11 @@ export const emergenciesList: EmergencyListItem[] = [
           1.Get someone to call 911 or the medical alert system for your location.
           2.Start chest compressions immediately. Push down hard and fast in the center of the chest, allowing the chest to come back up naturally between compressions. Continue until someone with more training has arrived.
           3.If you're trained in CPR, use chest compressions and rescue breathing.
-          4.Use an AED if one is available. It's important to not delay chest compressions, though, so have someone else find one while you do chest compressions.`,
+          4.Use an AED if one is available. It's important to not delay chest compressions, though, so have someone else find one while you do chest compressions.
+          
+          
+          Source: https://www.verywellhealth.com/basic-first-aid-procedures-1298578
+          `,
         stepImageUrl:
           'https://www.verywellhealth.com/thmb/pB5c6N4dkpbdy2XJ4bIeiAz8i8c=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-505252032-56d1ec685f9b5879cc81553d.jpg',
       },
@@ -116,7 +162,11 @@ export const emergenciesList: EmergencyListItem[] = [
           3.Apply petroleum jelly.
           4.Bandage it.
 
-          Change the bandage any time it gets wet. Take it off when you go to bed so the area can dry. `,
+          Change the bandage any time it gets wet. Take it off when you go to bed so the area can dry. 
+          
+          
+          Source: https://www.verywellhealth.com/basic-first-aid-procedures-1298578
+          `,
         stepImageUrl:
           'https://www.verywellhealth.com/thmb/-umtZsKRfpvxVSwA2tH64DQAGX0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/cropped-hand-of-man-applying-adhesive-bandage-on-ankle-1187311628-5b58a1be0a874e0586204d6b85ea8c55.jpg',
       },
@@ -136,7 +186,11 @@ export const emergenciesList: EmergencyListItem[] = [
           3.Take ibuprofen or acetaminophen for pain relief if necessary.
           4.Do not break any blisters that may have formed.
           
-          Do not apply ointments, butter, or oily remedies to a burn.Do not apply ointments, butter, or oily remedies to a burn.`,
+          Do not apply ointments, butter, or oily remedies to a burn.Do not apply ointments, butter, or oily remedies to a burn.
+          
+          
+          Source: https://www.verywellhealth.com/basic-first-aid-procedures-1298578
+          `,
         stepImageUrl:
           'https://previews.123rf.com/images/fordzolo/fordzolo1506/fordzolo150600296/41026708-example-white-stamp-text-on-red-backgroud.jpg',
       },
@@ -155,7 +209,11 @@ export const emergenciesList: EmergencyListItem[] = [
           2.For a limb, use a splint and padding to immobilize it.
           3.Put a cold pack on the injury, with a barrier between it and the skin to prevent tissue damage. If ice is all that's available, put it in a plastic bag and wrap it in a shirt or towel.
           4.If it's an extremity, elevate it.
-          5.Give anti-inflammatory drugs like Advil (ibuprofen) or Aleve (naproxen) for pain. `,
+          5.Give anti-inflammatory drugs like Advil (ibuprofen) or Aleve (naproxen) for pain. 
+          
+          
+          Source: https://www.verywellhealth.com/basic-first-aid-procedures-1298578
+          `,
         stepImageUrl:
           'https://www.verywellhealth.com/thmb/1k9j5CJh2YVHX_QjozDcmYrRB-c=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/woman-sitting-with-ice-pack-on-knee-146276242-577c320f3df78cb62c9009cf.jpg',
       },
@@ -175,7 +233,11 @@ export const emergenciesList: EmergencyListItem[] = [
           3.Elevate the injured part if you can do so safely
           4.Use NSAIDs for pain
           
-          See your healthcare provider soon for further diagnosis and treatment. `,
+          See your healthcare provider soon for further diagnosis and treatment. 
+          
+          
+          Source: https://www.verywellhealth.com/basic-first-aid-procedures-1298578
+          `,
         stepImageUrl:
           'https://www.verywellhealth.com/thmb/tLEtE6eh1D4128g251s9Nis-iPc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-580815229-56b05b583df78cf772ce6891.jpg',
       },
@@ -193,7 +255,11 @@ export const emergenciesList: EmergencyListItem[] = [
           1.Lean slightly forward, not back.
           2.Pinch the nose just below the bridge, high enough that the nostrils aren't pinched closed.
           3.Check after five minutes to see if bleeding has stopped. If not, continue pinching and check after another 10 minutes.
-          4.You can also apply a cold pack to the bridge of the nose while pinching. `,
+          4.You can also apply a cold pack to the bridge of the nose while pinching. 
+          
+          
+          Source: https://www.verywellhealth.com/basic-first-aid-procedures-1298578
+          `,
         stepImageUrl:
           'https://www.verywellhealth.com/thmb/0T895cqWoarQAlU7r2epeM31ea4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-565976469-570285db3df78c7d9e6d8feb.jpg',
       },
@@ -219,7 +285,11 @@ export const emergenciesList: EmergencyListItem[] = [
           
           For small areas of minor frostbite, you can also warm the area with skin-to-skin contact.
           
-          Get emergency treatment if the skin is hard and begins turning white. `,
+          Get emergency treatment if the skin is hard and begins turning white. 
+          
+          
+          Source: https://www.verywellhealth.com/basic-first-aid-procedures-1298578
+          `,
         stepImageUrl:
           'https://cdn.britannica.com/72/130172-050-1E10CDDC/hands.jpg',
       },
@@ -238,9 +308,69 @@ export const emergenciesList: EmergencyListItem[] = [
           2.Wash the area with soap and water.
           3.Use a cold pack to reduce swelling at the site, but don't apply ice directly to the skin. 
           4.Use an antihistamine like Benadryl (diphenhydramine) to reduce swelling and itching. 
-          5.Use Advil or Tylenol for pain. `,
+          5.Use Advil or Tylenol for pain. 
+          
+          
+          Source: https://www.verywellhealth.com/basic-first-aid-procedures-1298578
+          `,
         stepImageUrl:
           'https://www.verywellhealth.com/thmb/bq-XUm1SoeuObKOm0JGRqT3gbyI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-184988794-570278d83df78c7d9e6c1244.jpg',
+      },
+    ],
+  },
+  {
+    title: 'Self Defense',
+    icon: 'sports-mma',
+    steps: [
+      {
+        stepName: 'Self Defense Instructions',
+        stepDescription: dedent`SELF DEFENSE
+        Gun Defense
+        Your first line of defense with a gun is either going to be your legs or your voice depending on
+        the type of situation and conflict that the gun is drawn in. Depending on the situation, if you
+        are able to evade the situation by running this is always wise, but sometimes guns can be
+        drawn when people are with their families, out with their friends or loved ones and it may not
+        be smart to leave these people behind and run. If you can’t run, try and deescalate the
+        situation by talking and giving into the demands of the assailant, assuming they are not
+        hazardous or horrific. If all they want is your watch and wallet give it to them. No possession
+        is worth risking your life, if they want something more terrible like your child, you may have
+        to take action.
+
+        When confronting a gun with physical defense there should be two primary goals at
+        hand. The first goal should be to get yourself out of the line of fire, if you are with loved ones
+        or friends, get them out of the line of fire as well. How is this accomplished? Well you need to
+        grab the barrel of the weapon strategically and move it so no one is in the line of fire. This is
+        extremely important. After successfully getting yourself out of the line of fire, you will want to
+        proceed to disarm your assailant. Disarming somebody is a lot easier said than done and
+        hopefully you will never find yourself in a position where it is necessary to have to do this but
+        you never know.
+
+        Watch video for Reference https://youtu.be/Od4D7YJWtEo
+        
+
+        Knife Defense
+        Knives are some of the most dangerous weapons and much like with a gun the best defense
+        is evasion and de-escalation. You want to try and run or talk your way out of it. We all know
+        that this is much easier said than done and that sometimes it is unrealistic to think we can
+        talk or run our way out of a situation. So what if we can’t? What if we are with our family or
+        our children and we can’t just leave them there while we run?
+
+        When you are dealing with a knife, much like with a gun, you want to get out of the line of
+        danger; you need to control your attackers arm. This should be the primary focus when
+        dealing with a knife. Get a hold of their arm and control where that knife goes. Do not try and
+        take them down, punch them or kick them. Address the problem at hand, which is the
+        knife. Knives can cause an extreme amount of damage, you don’t want to wrestle around
+        with your attacker because you can get cut or stabbed, you also don’t want to stand in front
+        of them and try to fight them traditionally, you can also get cut or stabbed.
+        
+        
+        Watch the video for reference https://youtu.be/Mw558qhGY8s
+
+        Self Defense against Unarmed Attacks
+        https://www.maxkravmaga.com/video/defending-unarmed-attacks-introduction-a-word-from-eyal/
+        `,
+        stepImageUrl:
+          'https://previews.123rf.com/images/fordzolo/fordzolo1506/fordzolo150600296/41026708-example-white-stamp-text-on-red-backgroud.jpg',
       },
     ],
   },
