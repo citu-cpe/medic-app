525bf0241a6a46c7ef363168cffe9159cdc5ef13
Princh Canal <princhcanal@gmail.com>
2021-10-14 13:11:41 +0800

    feat(emergency numbers): add modal for hospital, police station, and fire station emergency numbers

---
+++ data/emergencyNumbers/fireStationEmergencyNumbers.ts
@@ -0,0 +1,9 @@
+import { EmergencyNumber } from '../../screens/HomeScreen/EmergencyNumbersModal/EmergencyNumbersModal';
+
+export const fireStationEmergencyNumbers: EmergencyNumber[] = [
+  {
+    name: 'Fire Station',
+    area: 'Minglanilla',
+    number: '999-999-9999',
+  },
+];
---
+++ screens/HomeScreen/EmergencyNumbersModal/EmergencyNumberCard/EmergencyNumberCard.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { EmergencyNumber } from '../EmergencyNumbersModal';
+import call from 'react-native-phone-call';
+import { Box } from 'native-base';
+import { TouchableOpacity } from 'react-native';
+import { colors } from '../../../../utils/colors';
+import { Text } from 'native-base';
+import { MaterialIcons } from '@expo/vector-icons';
+
+interface EmergencyNumberCardProps {
+  emergencyNumber: EmergencyNumber;
+}
+
+export const EmergencyNumberCard = (props: EmergencyNumberCardProps) => {
+  return (
+    <Box
+      flexDirection='row'
+      alignContent='center'
+      justifyContent='space-between'
+      my={1}
+      p={3}
+      bgColor={colors.white}
+      borderRadius={10}
+      shadow={3}
+    >
+      <Box flexDirection='row'>
+        <Box justifyContent='space-evenly'>
+          <Text fontWeight='bold' color={colors.black}>
+            {props.emergencyNumber.name}
+          </Text>
+          <Text>{props.emergencyNumber.number}</Text>
+        </Box>
+      </Box>
+
+      <Box
+        flexDirection='row'
+        alignItems='center'
+        justifyContent='space-between'
+      >
+        <TouchableOpacity
+          onPress={() =>
+            call({ number: props.emergencyNumber.number, prompt: true })
+          }
+        >
+          <MaterialIcons name='phone' size={40} color={colors.red} />
+        </TouchableOpacity>
+      </Box>
+    </Box>
+  );
+};
